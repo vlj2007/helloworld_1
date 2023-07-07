49edca668b607deba49acdc88aea1c687df02497
vlj2007 <vlj2007@yandex.ru>
2023-07-07 20:42:51 +0700

    Add new symbols

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     //test
     //test1
     public static void main(String[] args) {
-        System.out.println("Привет, Мир");
+        System.out.println("Привет, Мир!!!!!!!");
     }
 }
